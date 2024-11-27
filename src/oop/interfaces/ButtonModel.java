0135ac8b02d164fe59aece8b92e1717714348090
Dex4short <dextepacanya@gmail.com>
2024-11-27 18:11:07 +0800

    Delete src directory

--- src/oop/interfaces/ButtonModel.java
+++
@@ -1,9 +0,0 @@
-package oop.interfaces;
-
-public interface ButtonModel {
-
-	public boolean isHovered();
-	public void setHovered(boolean isHovered);
-	public boolean isPressed();
-	public void setPressed(boolean isPressed);
-}
--- src/oop/interfaces/Miscellaneous.java
+++
@@ -1,7 +0,0 @@
-package oop.interfaces;
-
-import java.awt.Component;
-
-public interface Miscellaneous {
-	public void apply(Component component);
-}
